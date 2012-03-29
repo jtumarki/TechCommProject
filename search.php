ea069a2043e14449aa801e810365ac1f35e8c1b0
Tyler Hedrick <tidus575@gmail.com>
2012-03-29 12:25:07 -0400

    put the search.php file in the same directory as index.html to make it easier to access. Added a simple comments.html page that currently links to Disqus but we may want to try using facebook instead.

---
+++ search.php
@@ -0,0 +1,28 @@
+<?php
+$con = mysql_connect ("logindbthedrick.db.7857300.hostedresource.com", "logindbthedrick","JsJt20!2tc221");
+mysql_select_db ("logindbthedrick", $con);
+ 
+$term = $_POST['term'];
+$parsed = str_replace('-','',$term);
+ 
+$sql = mysql_query("select * from classData where course_num like '%$parsed%' or title like '%$term%' or instructor like '%$term%' or semester like '$term'");
+
+echo '<table>';
+echo '<tr><td>Course</td><td>Title</td><td>Units</td><td>Section</td><td>Time</td><td></td><td>Room/Bldg</td><td>Instructor</td><td>Semester</td></tr>';
+
+while ($row = mysql_fetch_array($sql)){
+	echo '<tr>';
+	echo '<td> '.$row['course_num'].' </td>';
+    echo '<td> '.$row['title'].' </td>';
+    echo '<td> '.$row['units'].' </td>';
+    echo '<td> '.$row['section'].' </td>';
+    echo '<td> '.$row['begin'].' </td>';
+    echo '<td> '.$row['end'].' </td>';
+    echo '<td> '.$row['room'].' </td>';
+    echo '<td> '.$row['instructor'].' </td>';
+    echo '<td> '.$row['semester'].' </td>'; 
+    echo '</tr>';
+    }
+echo '</tr>';
+echo '</table>';
+?>
