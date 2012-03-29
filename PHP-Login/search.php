a543cf35ade6b71c9b33ceb0ff3a01eced56a15f
Tyler Hedrick <tidus575@gmail.com>
2012-03-29 10:00:20 -0400

    Ok this should be the last commit for the table stuff in search.php

--- PHP-Login/search.php
+++ PHP-Login/search.php
@@ -8,13 +8,13 @@ $parsed = str_replace('-','',$term);
 $sql = mysql_query("select * from classData where course_num like '%$parsed%' or title like '%$term%' or instructor like '%$term%' or semester like '$term'");
 
 echo '<table>';
-echo '<tr><td>Course</td><td>Title</td><td>Units</td><td>Section</td><td>Time</td><td>Room/Bldg</td><td>Instructor</td><td>Semester</td></tr>';
+echo '<tr><td>Course</td><td>Title</td><td>Units</td><td>Section</td><td>Time</td><td></td><td>Room/Bldg</td><td>Instructor</td><td>Semester</td></tr>';
 
 while ($row = mysql_fetch_array($sql)){
 	echo '<tr>';
-	echo '<td> '.$row['course_num'].' </td';
+	echo '<td> '.$row['course_num'].' </td>';
     echo '<td> '.$row['title'].' </td>';
-    echo '<td> '.$row['units'].' </td';
+    echo '<td> '.$row['units'].' </td>';
     echo '<td> '.$row['section'].' </td>';
     echo '<td> '.$row['begin'].' </td>';
     echo '<td> '.$row['end'].' </td>';
