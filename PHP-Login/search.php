12c43461e4ccd19aca37beaf110291e084650dac
Tyler Hedrick <tidus575@gmail.com>
2012-03-29 09:51:07 -0400

    Fixed the search.php script (bugs)

--- PHP-Login/search.php
+++ PHP-Login/search.php
@@ -6,22 +6,23 @@ $term = $_POST['term'];
 $parsed = str_replace('-','',$term);
  
 $sql = mysql_query("select * from classData where course_num like '%$parsed%' or title like '%$term%' or instructor like '%$term%' or semester like '$term'");
- 
+
+echo '<table>';
 echo '<tr><td>Course</td><td>Title</td><td>Units</td><td>Section</td><td>Time</td><td>Room/Bldg</td><td>Instructor</td><td>Semester</td></tr>';
 
 while ($row = mysql_fetch_array($sql)){
-	echo '<tr>'
-	echo '<td>'.$row['course_num'].'</td';
-    echo '<td>'.$row['title'].'</td>';
-    echo '<td>'.$row['units'].'</td';
-    echo '<td>'.$row['section'].'</td>';
-    echo '<td>'.$row['begin'].'</td>';
-    echo '<td>'.$row['end'].'</td>';
-    echo '<td>'.$row['room'].'</td>';
-    echo '<td>'.$row['instructor'].'</td>';
-    echo '<td>'.$row['semester'].'</td>'; 
+	echo '<tr>';
+	echo '<td> '.$row['course_num'].' </td';
+    echo '<td> '.$row['title'].' </td>';
+    echo '<td> '.$row['units'].' </td';
+    echo '<td> '.$row['section'].' </td>';
+    echo '<td> '.$row['begin'].' </td>';
+    echo '<td> '.$row['end'].' </td>';
+    echo '<td> '.$row['room'].' </td>';
+    echo '<td> '.$row['instructor'].' </td>';
+    echo '<td> '.$row['semester'].' </td>'; 
     echo '</tr>';
     }
 echo '</tr>';
- 
+echo '</table>';
 ?>
