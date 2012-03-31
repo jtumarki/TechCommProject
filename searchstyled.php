4965c4b77c9716865260d02c2fe66fab8bdd25ac
unknown <Jennifer@ToastBot.(none)>
2012-03-30 22:19:22 -0400

    Stylized search and related files

---
+++ searchstyled.php
@@ -0,0 +1,54 @@
+<?php
+$con = mysql_connect ("logindbthedrick.db.7857300.hostedresource.com", "logindbthedrick","JsJt20!2tc221");
+mysql_select_db ("logindbthedrick", $con);
+ 
+$term = $_POST['term'];
+$parsed = str_replace('-','',$term);
+ 
+$sql = mysql_query("select * from classData where course_num like '%$parsed%' or title like '%$term%' or semester like '$term'");
+
+include 'searchresults_header.php';
+echo 'Showing # results for ';
+echo $term;
+echo '... </ br>';
+echo '</div>
+		<div class="left">
+			<h2>Courses:</h2>';
+echo '<table>';
+while ($row = mysql_fetch_array($sql)){
+	echo '<tr>';
+	echo '<td> '.$row['course_num'].' </td>';
+    echo '<td> '.$row['title'].' </td>';
+    echo '<td> '.$row['units'].' </td>';
+    echo '<td> '.$row['begin'].' </td>';
+    echo '<td> '.$row['end'].' </td>';
+    echo '<td> '.$row['room'].' </td>';
+    echo '<td> '.$row['instructor'].' </td>';
+    echo '<td> '.$row['semester'].' </td>'; 
+    echo '</tr>';
+    }
+echo '</table>';
+
+echo '</div>
+		<div class="right">
+			<h2>Professors:</h2>';
+			
+$sql = mysql_query("select * from classData where instructor like '%$term%'");
+echo '<table>';
+while ($row = mysql_fetch_array($sql)){
+	echo '<tr>';
+	echo '<td> '.$row['course_num'].' </td>';
+    echo '<td> '.$row['title'].' </td>';
+    echo '<td> '.$row['units'].' </td>';
+    echo '<td> '.$row['section'].' </td>';
+    echo '<td> '.$row['begin'].' </td>';
+    echo '<td> '.$row['end'].' </td>';
+    echo '<td> '.$row['room'].' </td>';
+    echo '<td> '.$row['instructor'].' </td>';
+    echo '<td> '.$row['semester'].' </td>'; 
+    echo '</tr>';
+    }
+echo '</table>';
+
+include 'searchresults_footer.php';
+?>
