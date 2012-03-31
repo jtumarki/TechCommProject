0e00865d120a282eac453b5c4baa74a6f9d376f8
Tyler Hedrick <tidus575@gmail.com>
2012-03-30 21:57:32 -0400

    Updated a bunch of php files.

---
+++ comments.php
@@ -0,0 +1,18 @@
+<?php
+	require_once('auth.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Comment Shit</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Tyler Hedrick">
+	<!-- Date: 2012-03-29 -->
+</head>
+<body>
+	These are comments.
+	
+	<?php echo $_SESSION['SESS_FIRST_NAME']; ?>
+</body>
+</html>
