f49fcfcd51b36d1cf22c7e4e3271f080a61f008b
Tyler Hedrick <tidus575@gmail.com>
2012-04-02 19:42:08 -0400

    Added the faculty scrape python code

---
+++ facultyScrape.py
@@ -0,0 +1,34 @@
+import httplib2
+from BeautifulSoup import BeautifulSoup
+
+cache = httplib2.Http(".cache")
+
+def get_faculty():
+    http = "http://www.tylerhedrick.com/tylerhedrick/faculty.html"
+    page = cache.request(http,"GET")
+    
+    soup = BeautifulSoup(page[1])
+    faculty = soup.findAll('a')
+    fnames = []
+    lnames = []
+    depts = soup.findAll('td')
+    for n in range(1,len(faculty)-1,2):
+        fnames.append(faculty[n+1].string)
+        lnames.append(faculty[n].string)   
+    deps = []
+    for d in range(4,len(depts),4):
+        deps.append(str(str(depts[d].string).split()[0]))
+    a = []
+    for item in range(3,len(depts),4):
+        final = ''
+        l = str(depts[item].string).replace('&amp;','').replace('\n','').split()
+        for s in l:
+            final = final + s + " "
+        a.append(final[:-1])
+    return (fnames,lnames,a)
+        
+    
+(f,l,a) = get_faculty()
+
+for x in xrange(len(a)):
+    print 'mysql_query("INSERT INTO instructors (fname,lname,dept) VALUES (' + "'" + f[x] + "'"+ ",'" + l[x] + "'" + ",'" + a[x] + "'"+ ')");'
