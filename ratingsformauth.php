b7be2685807b47cb29b878950e0c9e7b44490c90
unknown <Jessica@Jessica-PC.(none)>
2012-04-02 22:05:35 -0400

    stuff

---
+++ ratingsformauth.php
@@ -0,0 +1,11 @@
+<?PHP
+
+session_start();
+	if (!isset($_SESSION['SESS_MEMBER_ID']){
+		echo 'Please login to comment.';
+	}
+	else {
+		echo 'Go ahead and leave a comment!';
+		include "ratingsform.php";
+	}
+?>
