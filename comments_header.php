43d51ffe87b9001f2d4370c9a8825f880b4f3d29
unknown <Jennifer@ToastBot.(none)>
2012-04-16 20:37:09 -0400

    fixed?

--- comments_header.php
+++ comments_header.php
@@ -5,10 +5,10 @@
 <title>Best Faculty Search - 15251 - Great Theoretical Ideas in Computer Science</title>
 <link rel="stylesheet" type="text/css" href="style.css" />
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
-<script src=\'jquery.js\' type="text/javascript"></script>
-<script src=\'jquery.MetaData.js\' type="text/javascript" language="javascript"></script>
-<script src=\'jquery.rating.js\' type="text/javascript" language="javascript"></script>
-<link href=\'jquery.rating.css\' type="text/css" rel="stylesheet"/>
+<script src="jquery.js" type="text/javascript"></script>
+<script src="jquery.MetaData.js" type="text/javascript" language="javascript"></script>
+<script src="jquery.rating.js" type="text/javascript" language="javascript"></script>
+<link href="query.rating.css" type="text/css" rel="stylesheet"/>
 <style type="text/css">
 h2, h3, h4, h5, h6 {font-size: 0px;}
 .up {
